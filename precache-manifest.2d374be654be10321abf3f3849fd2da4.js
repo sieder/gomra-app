70ed2683e16df0f821d9a990d8fbc3c14be4845b
sieder <sieder00@gmail.com>
2019-10-06 12:22:58 +0300

    Updates

---
+++ precache-manifest.2d374be654be10321abf3f3849fd2da4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92775e8aad854ed2d9f212267a1582f8",
+    "url": "/gomra-app/index.html"
+  },
+  {
+    "revision": "794924159798c86ffdaa",
+    "url": "/gomra-app/static/css/main.b47c9d09.chunk.css"
+  },
+  {
+    "revision": "8a7a66cb178bfe2295d1",
+    "url": "/gomra-app/static/js/2.5791a056.chunk.js"
+  },
+  {
+    "revision": "794924159798c86ffdaa",
+    "url": "/gomra-app/static/js/main.c76d798a.chunk.js"
+  },
+  {
+    "revision": "1c9b3a12431600d61447",
+    "url": "/gomra-app/static/js/runtime~main.401ebc19.js"
+  },
+  {
+    "revision": "aab28eaafdb493f80dd8938d54368daf",
+    "url": "/gomra-app/static/media/Brisk-Pro-Thin.aab28eaa.otf"
+  },
+  {
+    "revision": "39f683e05ccc57d9ce32fd635d23a49d",
+    "url": "/gomra-app/static/media/moonlight.39f683e0.svg"
+  },
+  {
+    "revision": "e4b31a6af83c573847055a1feacdd043",
+    "url": "/gomra-app/static/media/password-hide.e4b31a6a.svg"
+  },
+  {
+    "revision": "ace0a56e0457ca3e4925ff3959cd5040",
+    "url": "/gomra-app/static/media/password-show.ace0a56e.svg"
+  },
+  {
+    "revision": "bf812ffb655b1a7b2aa8610bd54fbf24",
+    "url": "/gomra-app/static/media/search-icon.bf812ffb.svg"
+  }
+]);
